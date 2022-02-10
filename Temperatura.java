c88f5411622507ee8d10a2c5a5e24b6e81891a9a
Sylvia Missio <59312305+SMissio@users.noreply.github.com>
2022-02-10 12:27:05 -0300

    Add files via upload

---
+++ Temperatura.java
@@ -0,0 +1,11 @@
+package fundamentos;
+
+public class Temperatura {
+	public static void main(String[] args) {
+	final double val = 32;
+	double F = 84;
+	double c;
+	c = (F - val)* 5.0/9;
+	System.out.println("temperatura = " + c);
+}
+}
